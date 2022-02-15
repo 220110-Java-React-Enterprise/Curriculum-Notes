9e6c808fd3fea1f13810c1c9262b9d8c3e745fd8
Kyle <Kyle.Plummer@revature.com>
2022-02-15 13:48:34 -0600

    Initial commit of new spring project ToDoSpring

---
+++ demos/ToDoSpring/src/main/java/com/revature/ToDoSpring/ToDoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ToDoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoSpringApplication.class, args);
+	}
+
+}
---
+++ demos/ToDoSpring/src/test/java/com/revature/ToDoSpring/ToDoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ToDoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
